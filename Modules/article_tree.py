59ede1f39682e795d80d312cb8ede81d7c94d193
Osmanilge <o.ilge.unaldi@hotmail.com>
2024-03-18 01:28:27 +0300

    summarize from pdf is completed.

--- Modules/article_tree.py
+++ Modules/article_tree.py
@@ -1,6 +1,6 @@
 from transformers import pipeline
 from transformers import SummarizationPipeline
-
+import re
 def count_words(text):
     words = text.split()
     return len(words)
@@ -18,14 +18,40 @@ class ArticleTree:
     def print_article_tree(self):
         self.root.print_article_tree("")
 
+    def calculate_count_of_words(self):
+        self.root.calculate_count_of_words()
+
+    def print_count_words(self):
+        self.root.print_count_words("")
+    
+    def summarize_parts(self, ratio, tokenizer, model, elasticityOfLength = 0.4):
+        self.root.tokenize_node(tokenizer)
+
+        allText = ""
+        for child in self.root.children:
+            allText += child.data + '\n' + child.summarize_article_with_ratio(ratio, tokenizer, model, elasticityOfLength, "") + '\n'
+
+        return allText
+    
+    def summarize_parts_as_list(self, ratio, tokenizer, model, elasticityOfLength = 0.4):
+        self.root.tokenize_node(tokenizer)
+
+        parts = []
+        for child in self.root.children:
+            parts.append([child.data, child.summarize_article_with_ratio(ratio, tokenizer, model, elasticityOfLength, "")])
+
+        return parts
+
+
 class Node:
     def __init__(self, depth, id, data):
         self.depth = depth
-        self.id = id
-        self.data = data.replace("-\n", "").replace("\n", " ")
+        #id is None while the Node is external(Paragraph) node
+        self.id = id 
+        self.data = data.replace("-\r\n", "").replace("\r\n", " ").replace("-\n", "").replace("\n", " ")
         self.children = []
-        self.summarizeText = None
         self.countOfWords = 0
+        self.countOfTokens = 0
 
     def add_title_node(self, id_path, data):
         ids = id_path.split(".")
@@ -53,6 +79,17 @@ class Node:
         print(path," ",self.data)
         for child in self.children:
             child.print_article_tree(path+".")
+    
+    def get_text_from_node(self,threshold = 0):
+        result = ""
+        if self.id is None:
+            if self.countOfWords > threshold:
+                result= self.data
+        else:
+            for child in self.children:
+                result += child.get_text_from_node(threshold)+" "
+
+        return result
 
     def calculate_count_of_words(self):
         totalCount = 0
@@ -73,24 +110,146 @@ class Node:
         for child in self.children:
             child.print_count_words(path+".")
 
-    def summarize_article(self, summarize_length, Summarizer, elasticityOfLength = 0.1):
-
+    def tokenize_node(self, tokenizer):
+        totalCount = 0
         if self.id is not None:
-            allText = ""
             for child in self.children:
-                allText += child.summarize_article(int(1000*child.countOfWords/self.countOfWords), Summarizer, elasticityOfLength) + "\n\n"
+                totalCount += child.tokenize_node(tokenizer)
 
-            if summarize_length < self.countOfWords:
-                self.summarizeText = Summarizer(self.data, max_length=summarize_length, min_length=summarize_length*(1 - elasticityOfLength), do_sample=False) 
+        else:
+            tokenizedText = tokenize_text(self.data, tokenizer)
+            totalCount = len(tokenizedText[0])
+
+        self.countOfTokens = totalCount
+        return totalCount 
+    
+    def print_count_tokens(self, path):
+        print(path," ",self.countOfTokens)
+        if self.countOfTokens < 7:
+            print(self.data)
+            
+        for child in self.children:
+            child.print_count_words(path+".")
+        
+    def summarize_article_with_ratio(self, ratio, tokenizer, model, elasticityOfLength, id_path):
+        print("SummarizeR",self.depth," ",id_path + self.id)
+        result = ""
+        if self.id is not None:
+            if self.countOfTokens < 1024:
+                text = self.get_text_from_node(7)
+                cWord = count_words(text)
+                inputs = tokenize_text(text,tokenizer)
+                if len(inputs[0]) > 2:
+                    summary_ids = summarize_tokens(inputs, int(ratio*cWord), model, elasticityOfLength)
+                    result += detokenize_text(summary_ids, tokenizer) + '\n'
+                else:
+                    print("empty text tokenized!1")
+                return result
+
             else:
-                self.summarizeText = self.data
+                allText = ""
+                totalCountOfTokens = 0
+                for child in self.children:
+                    if totalCountOfTokens + child.countOfTokens > 1024 and totalCountOfTokens != 0:
+                        print("What we think that we send token", totalCountOfTokens)
+                        cWord = count_words(allText)
+                        inputs = tokenize_text(allText, tokenizer)
 
-            return self.summarizeText 
+                        if len(inputs[0]) > 2:
+                            summary_ids = summarize_tokens(inputs, int(ratio*cWord), model, elasticityOfLength)
+                            result += detokenize_text(summary_ids, tokenizer) + '\n'
+                        else:
+                            print("empty text tokenized!2")
+
+                        allText = ""
+                        totalCountOfTokens = 0
+                    
+                    if child.countOfTokens >= 1024:
+                        result += child.summarize_article_with_ratio(ratio, tokenizer, model, elasticityOfLength,  id_path + str(self.id) +".")
+                    
+                    else:
+                        allText += child.get_text_from_node(7)
+                        totalCountOfTokens += child.countOfTokens
+
+                if totalCountOfTokens <1024 and totalCountOfTokens != 0:
+                        print("What we think that we send token", totalCountOfTokens)
+                        cWord = count_words(allText)
+                        inputs = tokenize_text(allText, tokenizer)
+                        if len(inputs[0]) > 2:
+                            summary_ids = summarize_tokens(inputs, int(ratio*cWord), model, elasticityOfLength)
+                            result += detokenize_text(summary_ids, tokenizer) + '\n'
+                        else:
+                            print("empty text tokenized!3")
+
+                return result
+            
             
         else:
-            if summarize_length < self.countOfWords:
-                self.summarizeText = Summarizer(self.data, max_length=summarize_length, min_length=summarize_length*(1 - elasticityOfLength), do_sample=False) 
-            else:
-                self.summarizeText = self.data
+            if self.countOfTokens < 1024:
+                print("What we think that we send token", totalCountOfTokens)
+                cWord = count_words(self.data)
 
-            return self.summarizeText 
+                inputs = tokenize_text(self.data, tokenizer)
+                if len(inputs[0]) > 2:
+                    summary_ids = summarize_tokens(inputs, int(ratio*cWord), model, elasticityOfLength)
+                    result += detokenize_text(summary_ids, tokenizer) + '\n'
+                else:
+                    print("empty text tokenized!4")
+
+                return result
+            
+            else:
+                strings = []
+                if self.countOfTokens < 2000:
+                    strings = divide_to_strings(self.data, int(self.countOfTokens / 2))
+                else:
+                    strings = divide_to_strings(self.data, 1000)
+
+                for text in strings:
+                    cWord = count_words(text)
+                    inputs = tokenize_text(text, tokenizer)
+
+                    if len(inputs[0]) > 2:
+                        summary_ids = summarize_tokens(inputs, int(ratio*cWord), model, elasticityOfLength)
+                        result += detokenize_text(summary_ids, tokenizer) + ' '
+
+                return result
+
+def divide_to_strings(string, size):
+    words = string.split()
+    groups = []
+    for i in range(0, len(words), size):
+        group = ' '.join(words[i:i+size])
+        groups.append(group)
+    return groups            
+
+def summarize_tokens(inputs, summarize_length, model, elasticityOfLength):
+    summary_ids = None
+    try:
+        #re.sub(r'\n+', r'\n', text)
+        #result = model(text, max_length=summarize_length, min_length=int(summarize_length*(1 - elasticityOfLength)), do_sample=False)[0]['summary_text']
+        summary_ids = model.generate(inputs["input_ids"], num_beams=2, min_length=int(summarize_length*(1 - elasticityOfLength)), max_length=summarize_length)
+    except Exception as e:
+        print("!Error occurred while summarizing tokens:", e)
+        print(inputs,"\n")
+    return summary_ids
+
+def tokenize_text(text, tokenizer):
+    inputs = None
+    try:
+        #re.sub(r'\n+', r'\n', text)
+        inputs = tokenizer([text], max_length=1024, truncation=True, return_tensors="pt")
+    except Exception as e:
+        print("!Error occurred while tokenize:", e)
+        print(text,"\n")
+    return inputs
+
+def detokenize_text(summary_ids, tokenizer):
+    result = None
+    try:
+        #re.sub(r'\n+', r'\n', text)
+        result = tokenizer.batch_decode(summary_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+    except Exception as e:
+        print("!Error occurred while detokenize:", e)
+        print(summary_ids,"\n")
+    return result
--- Modules/reader.py
+++ Modules/reader.py
@@ -1,48 +1,236 @@
 from pdfminer.high_level import extract_pages
 from pdfminer.layout import LTTextBox,LTTextContainer, LTChar
 from Modules.article_tree import ArticleTree
+from enum import Enum
 import re
 
-def find_pre_pattern(text):
-    pattern = r'[1-9](\.[1-9])*\n'  # Aradığınız regex deseni
-    match = re.match(pattern, text)
-    return bool(match)
+class TitleForm(Enum):
+    ROME = 1            #I  Introduction
+    ROMEwPOINT_end = 2  #I. Introduction
+    NUM = 3             #1  Introduction
+    NUMwPOINT_end = 4   #1. Introduction
+    TYPELESS = 5
 
-def find_custom_pattern(text):
-    pattern = r'[1-9](\.[1-9])*\s[a-zA-Z].*?\n'  # Güncellenmiş regex deseni
-    match = re.match(pattern, text)
-    return bool(match)
+def isTitle(text, titleType):
+
+    #Intiliaze
+    partTwo = text
+    if(titleType == TitleForm.TYPELESS and len(text.split()) > 1):
+        partTwo = text.split()[1]
+
+    if(titleType == TitleForm.TYPELESS and bool(re.match("introduction",partTwo.lower()))):
+
+        pattern = r'([IVX]+(\.[IVX]+)*)\s+[a-zA-Z].*?\n'  
+        match = re.match(pattern, text)
+        if(bool(match)):
+            return bool(match), TitleForm.ROME
+        
+        pattern = r'(([IVX]+\.)+)\s+[a-zA-Z].*?\n'  
+        match = re.match(pattern, text)
+        if(bool(match)):
+            return bool(match), TitleForm.ROMEwPOINT_end
+        
+        pattern = r'([1-9]+(\.[1-9]+)*)\s+[a-zA-Z].*?\n'  
+        match = re.match(pattern, text)
+        if(bool(match)):
+            return bool(match), TitleForm.NUM
+    
+        pattern = r'(([1-9]+\.)+)\s+[a-zA-Z].*?\n'  
+        match = re.match(pattern, text)
+        if(bool(match)):
+            return bool(match), TitleForm.NUMwPOINT_end
+
+    #After Intiliaze
+
+    if(titleType == TitleForm.ROME):
+        pattern = r'([IVX]+(\.[IVX]+)*)\s+[a-zA-Z].*?\n'  
+        match = re.match(pattern, text)
+        return bool(match), TitleForm.ROME
+
+    elif(titleType == TitleForm.ROMEwPOINT_end):
+        pattern = r'(([IVX]+\.)+)\s+[a-zA-Z].*?\n'  
+        match = re.match(pattern, text)
+        return bool(match), TitleForm.ROMEwPOINT_end
+    
+    elif(titleType == TitleForm.NUM):
+        pattern = r'([1-9]+(\.[1-9]+)*)\s+[a-zA-Z].*?\n'  
+        match = re.match(pattern, text)
+        return bool(match), TitleForm.NUM
+    
+    elif(titleType == TitleForm.NUMwPOINT_end):
+        pattern = r'(([1-9]+\.)+)\s+[a-zA-Z].*?\n'  
+        match = re.match(pattern, text)
+        return bool(match), TitleForm.NUMwPOINT_end
+    
+    return False, TitleForm.TYPELESS
+
+"""
+    It is needed for that if title and order of title are divided into to two instance
+    Fact: Generally Introduction extracted like that, Reason: Idk
+"""
+def isTitlewPrev(prevelement, text, titleType):
+
+    #Intiliaze
+    if(titleType == TitleForm.TYPELESS and bool(re.match("introduction",text.lower()))):
+
+        pattern = r'([IVX]+(\.[IVX]+)*)\s*?\n'  
+        match = re.match(pattern, prevelement)
+        if(bool(match)):
+            return bool(match), TitleForm.ROME
+        
+        pattern = r'(([IVX]+\.)+)\s*?\n'  
+        match = re.match(pattern, prevelement)
+        if(bool(match)):
+            return bool(match), TitleForm.ROMEwPOINT_end
+        
+        pattern = r'([1-9]+(\.[1-9]+)*)\s*?\n'  
+        match = re.match(pattern, prevelement)
+        if(bool(match)):
+            return bool(match), TitleForm.NUM
+    
+        pattern = r'(([1-9]+\.)+)\s*?\n'  
+        match = re.match(pattern, prevelement)
+        if(bool(match)):
+            return bool(match), TitleForm.NUMwPOINT_end
+        
+
+    #After Intiliaze
+
+    if(titleType == TitleForm.ROME):
+        pattern = r'([IVX]+(\.[IVX]+)*)\s*?\n'  
+        match = re.match(pattern, prevelement)
+        return bool(match), TitleForm.ROME
+
+    elif(titleType == TitleForm.ROMEwPOINT_end):
+        pattern = r'(([IVX]+\.)+)\s*?\n'  
+        match = re.match(pattern, prevelement)
+        return bool(match), TitleForm.ROMEwPOINT_end
+    
+    elif(titleType == TitleForm.NUM):
+        pattern = r'([1-9]+(\.[1-9]+)*)\s*?\n'  
+        match = re.match(pattern, prevelement)
+        return bool(match), TitleForm.NUM
+    
+    elif(titleType == TitleForm.NUMwPOINT_end):
+        pattern = r'(([1-9]+\.)+)\s*?\n'  
+        match = re.match(pattern, prevelement)
+        return bool(match), TitleForm.NUMwPOINT_end
+    
+    return False, TitleForm.TYPELESS
+
+def roman_to_int(roman):
+    roman_dict = {'I': 1, 'IV': 4, 'V': 5, 'IX': 9, 'X': 10, 'XL': 40, 'L': 50, 'XC': 90, 'C': 100, 'CD': 400, 'D': 500, 'CM': 900, 'M': 1000}
+    result = 0
+    i = 0
+    while i < len(roman):
+        if i + 1 < len(roman) and roman[i:i+2] in roman_dict:
+            result += roman_dict[roman[i:i+2]]
+            i += 2
+        else:
+            result += roman_dict[roman[i]]
+            i += 1
+    return result
+
+def int_to_roman(num):
+    roman_numerals = {1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X', 40: 'XL', 50: 'L', 90: 'XC', 100: 'C', 400: 'CD', 500: 'D', 900: 'CM', 1000: 'M'}
+    result = ''
+    for value, numeral in sorted(roman_numerals.items(), reverse=True):
+        while num >= value:
+            result += numeral
+            num -= value
+    return result
+
+"""
+    Purpose: that function checks strictly that did the new title called in correct order. without this function, read work correctly but it makes more correct.
+    Side-efffect: if there is possibility one of title missed, then almost all titles related with that title will be gone. it is generally unexpected case.
+"""
+def isPathNext(path, pathNext, typeOfTitleForm):
+    if(path == ""):
+        return True
+
+    if(TitleForm.ROMEwPOINT_end == typeOfTitleForm or TitleForm.ROME == typeOfTitleForm):
+        Curr = [roman_to_int(x) for x in path.split('.')]
+        Next = [roman_to_int(x) for x in pathNext.split('.')]
+    elif(TitleForm.NUMwPOINT_end == typeOfTitleForm or TitleForm.NUM == typeOfTitleForm):
+        Curr = [int(x) for x in path.split('.')]
+        Next = [int(x) for x in pathNext.split('.')]
+    else:
+        return True
+
+    minimumRange = min(len(Next),len(Curr))
+    for i in range(minimumRange-1):
+        if(Curr[i] != Next[i]):
+            return False
+        
+    if(Curr[minimumRange - 1] + 1 == Next[minimumRange - 1] or Curr[minimumRange - 1] == Next[minimumRange - 1] and len(Next) > minimumRange and Next[minimumRange] == 1):
+        return True
+    else:
+        return False
+
+    
+    
 
 def get_tree_from_article_pdf(pdf_path):
     prevelement = None
     id_path = ""
     title_able = False
     article = None
-    
+    TypeOfTitle = TitleForm.TYPELESS
+    possibleNameOfArticle =  []
     for page_layout in extract_pages(pdf_path):
         for element in page_layout:
             """
                 This part gets just texts if you want to get different data, you need to use -isinstance(element, ...)-
             """
             if isinstance(element, LTTextBox):
-                if not title_able:
-                    article = ArticleTree(element.get_text())
-                    title_able = True
-                else:
-                    #check is it title?
-                    if find_custom_pattern(element.get_text()):
-                        id_path = element.get_text().split(" ")[0]
-                        article.add_title_node(id_path, " ".join(element.get_text().split(" ")[1:]))
-        
-                    elif prevelement is not None and find_pre_pattern(prevelement.get_text()) and prevelement.y0 == element.y0 and prevelement.y1 == element.y1:
-                        id_path = prevelement.get_text()[:-1] 
-                        article.add_title_node(id_path, element.get_text())
-                    #check is it title? end
 
-                    #check Introduction is seen before get the paragraph to tree  
-                    elif id_path != "":
-                        article.add_paragraph_node(id_path, element.get_text())
-                    #check Introduction is seen before get the paragraph to tree  end
+                if not title_able and len(element.get_text().split('\n')) > 1:
+                    possibleNameOfArticle.append([ element.height / (len(element.get_text().split('\n')) - 1), element ])
+
+
+                if len(element.get_text()) < len("References")+4 and re.match("references",element.get_text().lower()):
+                    print("References is detected.")
+                    return article
+                
+                #check is it title?
+                is_title, TypeOfTitle = isTitle(element.get_text(),TypeOfTitle)
+
+                is_prev = False
+                if not is_title and  prevelement is not None:
+                    is_prev, TypeOfTitle = isTitlewPrev(prevelement.get_text(), element.get_text(), TypeOfTitle)
+                    is_prev = is_prev and bool(re.match(r'\s*[a-zA-Z].*?\n',element.get_text()))
+
+                if  is_title:
+                    temp_id_path = element.get_text().split(" ")[0]
+                    temp_id_path = temp_id_path[:-1] if temp_id_path.endswith('.') else temp_id_path
+                    if(isPathNext(id_path,temp_id_path,TypeOfTitle)):
+                        if not title_able:
+                            nameOfArticle = sorted(possibleNameOfArticle, key=lambda x: x[0], reverse=True)[0][1].get_text()
+                            article = ArticleTree(re.sub(r'\n', r' ', nameOfArticle))
+                            print("Title",re.sub(r'\n', r' ', nameOfArticle))
+                            title_able = True
+
+                        id_path = temp_id_path
+                        article.add_title_node(id_path, " ".join(element.get_text().split(" ")[1:]))
+                        print("girdi2",element)
+
+                elif is_prev and prevelement.y0 == element.y0 and prevelement.y1 == element.y1:
+                    temp_id_path = prevelement.get_text()[:-1] 
+                    temp_id_path = temp_id_path[:-1] if temp_id_path.endswith('.') else temp_id_path
+                    if(isPathNext(id_path,temp_id_path,TypeOfTitle)):
+                        if not title_able:
+                            nameOfArticle = sorted(possibleNameOfArticle, key=lambda x: x[0], reverse=True)[0][1].get_text()
+                            article = ArticleTree(re.sub(r'\n', r' ', nameOfArticle))
+                            print("Title",re.sub(r'\n', r' ', nameOfArticle))
+                            title_able = True
+
+                        id_path = temp_id_path
+                        article.add_title_node(id_path, element.get_text())
+                        print("girdi3",prevelement, element)
+                #check is it title? end  
+                    
+                elif id_path != "":
+                    article.add_paragraph_node(id_path, element.get_text())
                         
                 prevelement = element
 
@@ -51,5 +239,5 @@ def get_tree_from_article_pdf(pdf_path):
             """
 
 
-
+    print("References is NOT detected.")
     return article
